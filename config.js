a2a5c4eaa928e7169abcabba9137ca5ff550d1bd
Ruben Gees <geesruben@yahoo.de>
2016-11-27 14:26:50 +0100

    Cleanups and minor improvements
    - All errors are now returned as JSON compliant with how GraphQL returns errors

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
-var config = {
-    auth: {}
-};
-
-config.auth.domain = process.env.AUTH_DOMAIN || "";
-config.auth.clientID = process.env.AUTH_CLIENT_ID || "";
-config.auth.clientSecret = process.env.AUTH_CLIENT_SECRET || "";
-
-module.exports = config;
+export default {
+    auth: {
+        domain: process.env.AUTH_DOMAIN || "",
+        clientID: process.env.AUTH_CLIENT_ID || "",
+        clientSecret: process.env.AUTH_CLIENT_SECRET || ""
+    }
+}
